06309227565044de9ad0ddc055282328e1bce2e9
hadramet <hadramet.sylla@outlook.fr>
2022-07-24 03:23:14 +0200

    update login api

--- pages/api/authentication/login.js
+++ pages/api/authentication/login.js
@@ -1,56 +1,48 @@
-import { compare } from "bcrypt"
-import clientPromise from "../../../lib/mongodb"
-import { withSessionRoute } from "../../../lib/session"
-
+import dbConnect from "../../../lib/mongodb";
+import { withSessionRoute } from "../../../lib/session";
+import User from "../../../models/user";
 
 async function Login(req, res) {
-    try {
+  const { method } = req;
+  await dbConnect();
 
-        // On considère que la forme de la requête est déjà validé
-        // car cette api n'a pas d'usage externe de prévu.
+  switch (method) {
+    case "POST":
+      try {
+        const user = await User.findOne({ email: req.body.email });
+        if (!user)
+          return res
+            .status(400)
+            .json({ success: false, message: "User not found" });
+        const match = user.validatePassword(req.body.password);
+        if (!match)
+          return res
+            .status(403)
+            .json({ success: false, message: "Invalid password" });
 
-        const client = await clientPromise
-        const db = client.db('userdb')
+        const session_user = {
+          id: user._id,
+          isLoggedIn: true,
+          infos: {
+            name: user.firstName +' '+ user.lastName,
+            email: user.email,
+            role: user.role,
+          },
+        };
 
-        const users = db.collection('users')
-        const roles = db.collection('roles')
+        req.session.user = session_user;
+        await req.session.save();
+        res.status(200).json({success: true, data: session_user});
 
-        if (req.method === "POST") {
-
-            const user = await users.findOne({ email: req.body.email })
-            if (!user) return res.status(403).send({ message: "Please check your email and password." })
-
-            const match = await compare(req.body.password, user.password)
-            if (!match) return res.status(403).send({ message: "Please check your email and password." })
-
-            const user_role = await roles.findOne({_id: user.roleId})
-
-            const session_user = {
-                id: user._id,
-                isLoggedIn: true,
-                infos:{
-                    name: user.name,
-                    email: user.email,
-                    role: user_role.name
-                }
-            }
-
-            req.session.user = session_user
-            await req.session.save()
-            
-            res.send({ session_user }) // just for test
-            res.status(200).end()
-        }
-        else
-            res.status(405).json({ message: "only post request are supported" }).end()
-
-
-    } catch (error) {
-        const message = ` Something went wrong : ${error}`
-        console.error('[Auth Api]', message)
-        res.status(500).json({ message: message })
-    }
+      } catch (error) {
+        console.log(error.message)
+        res.status(500).json({success: true});
+      }
+      break;
+    default:
+      res.status(400).json({success: true, message: "Only post request are supported" });
+      break;
+  }
 }
 
-
-export default withSessionRoute(Login)
+export default withSessionRoute(Login);
